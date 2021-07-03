d5c2e06dcefac0d9e9c2007c5df94332851d80c6
Cigan <ciganoliviudavid@gmail.com>
2021-07-03 23:03:52 +0300

    Setup apps and migrations

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWorkflow.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myworkflow.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
