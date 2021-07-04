8d80f4fa965ab5268d2caaa1eeaaa3c85e208318
Cigan <ciganoliviudavid@gmail.com>
2021-07-04 22:51:40 +0300

    Update models.py and migrations

--- activities/models.py
+++ activities/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from .data_structures import IMPORTANCE_LEVEL
+from projects.models import PersonalProject
 
 
 class CurrentActivitie(models.Model):
+    project = models.ForeignKey(PersonalProject, on_delete=models.CASCADE, default=0)
     activity = models.CharField(max_length=25, default="None", blank=False)
     importance_level = models.CharField(max_length=25, choices=IMPORTANCE_LEVEL, default="None", blank=False)
     short_description = models.TextField(default="None", blank=False)
