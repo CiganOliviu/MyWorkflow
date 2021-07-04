224f27f0abc94806fa6963df501a51da41b0095c
Cigan <ciganoliviudavid@gmail.com>
2021-07-04 22:30:30 +0300

    Update models.py and migrations

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrentActivitie(models.Model):
+    activity = models.CharField(max_length=25, default="None", blank=False)
+    importance_level = models.CharField(max_length=25, default="None", blank=False)
+    short_description = models.TextField(default="None", blank=False)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.activity
