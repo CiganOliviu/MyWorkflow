a104ead4f78195f92190ccd4c6d0e5c60ce0d312
Cigan <ciganoliviudavid@gmail.com>
2021-07-07 23:19:06 +0300

    Update models and migrations

--- activities/models.py
+++ activities/models.py
@@ -8,6 +8,7 @@ class CurrentActivitie(models.Model):
     activity = models.CharField(max_length=25, default="None", blank=False)
     importance_level = models.CharField(max_length=25, choices=IMPORTANCE_LEVEL, default="None", blank=False)
     short_description = models.TextField(default="None", blank=False)
+    deadline = models.DateTimeField()
     done = models.BooleanField(default=False)
 
     def __str__(self):
